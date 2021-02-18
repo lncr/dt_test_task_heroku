4c9c849d06ad1b2bebd9b2e5a9b6ee53edbb461c
Bektur Soltobaev <roctivosu@gmail.com>
2021-02-18 12:31:33 +0600

    Rewrite permissions

--- news/posts/views.py
+++ news/posts/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Comment, Post
 from .serializers import CommentSerializer, PostSerializer
-from .permissions import ReadOnly
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'id'
-    permission_classes = [IsAuthenticated | ReadOnly, ]
+    permission_classes = [IsAuthenticatedOrReadOnly & IsAuthorOrReadOnly, ]
 
 
 class CommentModelViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     lookup_field = 'id'
-    permission_classes = [IsAuthenticated | ReadOnly, ]
+    permission_classes = [IsAuthenticatedOrReadOnly & IsAuthorOrReadOnly, ]
 
     def get_queryset(self):
         post_id = self.request.kwargs.get('post_id')
